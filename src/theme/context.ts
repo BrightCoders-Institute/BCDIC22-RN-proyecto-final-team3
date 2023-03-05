e96c632c2ec3bf65e2be22d041d41c85d6ced34f
IvanGodinez21 <ivano.godinez@gmail.com>
2023-03-05 13:48:49 -0600

    refactor: :recycle: Modify theme context
    Modified:
    - Moved type used in the context to the types folder.

--- src/theme/context.ts
+++ src/theme/context.ts
@@ -1,34 +1,5 @@
 import { createContext } from 'react';
 import { themeMode } from './themes';
+import { IThemeContext } from '../types/theme/context';
 
-export default createContext<{
-  isDark?: boolean;
-  colors: {
-    background: string;
-    button: {
-      default: {
-        background: string;
-        text: string;
-      };
-    };
-    buttonIcon: {
-      default: {
-        background: string;
-        border: string;
-        text: string;
-      };
-    };
-    input: {
-      default: {
-        background: string;
-        text: string;
-        icon: string;
-        border: {
-          default: string;
-          active: string;
-        };
-      };
-    };
-    text: string;
-  };
-}>(themeMode.light || themeMode.dark);
+export default createContext<IThemeContext>(themeMode.light || themeMode.dark);
