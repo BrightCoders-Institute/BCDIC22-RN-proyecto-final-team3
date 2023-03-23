c2a2b6eef770f206240630cd6d0e9afc98bda894
EliasCode79 <eliasfranco2379@gmail.com>
2023-03-22 20:09:36 -0400

    refactor: :recycle: Move styles of Settings screen to corresponding file
    Modified :
    - Image move to assets.
    
    Co-Authored-By: McFly1208 <72634478+McFly1208@users.noreply.github.com>
    Co-Authored-By: Jesús Iván Godínez Martínez <ivano.godinez@gmail.com>

--- src/screens/Settings.tsx
+++ src/screens/Settings.tsx
@@ -2,13 +2,13 @@ import React, { Component } from 'react';
 import { Switch, View, Image } from 'react-native';
 import { DataTable } from 'react-native-paper';
 import { EventRegister } from 'react-native-event-listeners';
-import ThemeContext from '../theme/context';
+import CButton from '../components/CButton';
 import SettingsStyles from '../styles/screens/Settings';
+import ThemeContext from '../theme/context';
 import { ISettingsProps } from '../types/screens/Settings';
 import { IThemeContext } from '../types/theme/context';
-import CButton from '../components/CButton';
 
-export default class LogIn extends Component<ISettingsProps> {
+export default class Settings extends Component<ISettingsProps> {
   static contextType = ThemeContext;
   declare context: IThemeContext;
 
@@ -23,20 +23,32 @@ export default class LogIn extends Component<ISettingsProps> {
   render() {
     return (
       <View style={SettingsStyles(this.context).screen.style.container}>
-        <View style={{ justifyContent: 'center', alignItems: 'center', marginTop: 10 }}>
-          <Image style={{ width: 150, height: 150 }} source={require('../../img/usericon.png')} />
-        </View>
-        <View style={{ margin: 30 }}>
+        <View style={SettingsStyles(this.context).screen.style.content}>
+          <View style={SettingsStyles(this.context).screen.style.imageContainer}>
+            <Image style={SettingsStyles(this.context).screen.style.image} source={require('../assets/usericon.png')} />
+          </View>
           <DataTable>
-            <DataTable.Row>
-              <DataTable.Cell>DarkMode</DataTable.Cell>
-              <DataTable.Cell style={{ justifyContent: 'flex-end' }}>
+            <DataTable.Row style={SettingsStyles(this.context).screen.style.dividerOff}>
+              <DataTable.Cell
+                style={SettingsStyles(this.context).screen.style.cellCenter}
+                textStyle={SettingsStyles(this.context).screen.style.text}
+              >
+                example@email.com
+              </DataTable.Cell>
+            </DataTable.Row>
+            <DataTable.Row style={SettingsStyles(this.context).screen.style.dividerOff}>
+              <DataTable.Cell textStyle={SettingsStyles(this.context).screen.style.text}>DarkMode</DataTable.Cell>
+              <DataTable.Cell style={SettingsStyles(this.context).screen.style.cellEnd}>
                 <Switch value={this.context.isDark} onValueChange={this.onThemeChange} />
               </DataTable.Cell>
             </DataTable.Row>
-            <DataTable.Row>
-              <DataTable.Cell style={{ justifyContent: 'center' }}>
-                <CButton title='Logout' onPress={() => this.props.navigation.navigate('LogIn')} />
+            <DataTable.Row style={SettingsStyles(this.context).screen.style.dividerOff}>
+              <DataTable.Cell style={SettingsStyles(this.context).screen.style.cellCenter}>
+                <CButton
+                  style={SettingsStyles(this.context).logOutButton}
+                  title='Logout'
+                  onPress={() => this.props.navigation.navigate('LogIn')}
+                />
               </DataTable.Cell>
             </DataTable.Row>
           </DataTable>
--- src/styles/screens/Settings.ts
+++ src/styles/screens/Settings.ts
@@ -1,4 +1,6 @@
 import { StyleSheet } from 'react-native';
+import CButtonComponent from '../../components/CButton';
+import CButtonStyle from '../components/CButton';
 import { IThemeContext } from '../../types/theme/context';
 
 export default (context: IThemeContext) => {
@@ -8,10 +10,40 @@ export default (context: IThemeContext) => {
         container: {
           flex: 1,
         },
+        content: {
+          marginHorizontal: 20,
+        },
+        imageContainer: {
+          justifyContent: 'center',
+          alignItems: 'center',
+          marginTop: 10,
+        },
+        image: {
+          width: 150,
+          height: 150,
+        },
+        dividerOff: {
+          borderBottomWidth: 0,
+        },
+        cellEnd: {
+          justifyContent: 'flex-end',
+        },
+        cellCenter: {
+          justifyContent: 'center',
+        },
         text: {
           color: context.colors.text,
         },
       }),
     },
+    logOutButton: {
+      ...CButtonStyle(context),
+      buttonColor: 'transparent',
+      textColor: 'red',
+      icon: {
+        source: 'logout',
+        direction: 'ltr',
+      },
+    } as CButtonComponent['props']['style'],
   };
 };
