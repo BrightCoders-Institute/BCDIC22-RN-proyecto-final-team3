95d7815eaad1dfb657db2c7d6be95b19310a61c1
arthurchina92 <arthurchinaa@gmail.com>
2023-03-22 18:24:02 -0600

    feat: :sparkles: Add reusable components to details screen
    Added:
    - New types for the state.
    
    Co-Authored-By: Jesús Iván Godínez Martínez <ivano.godinez@gmail.com>
    Co-Authored-By: GERARDO ISMAEL GARCIA GONZALEZ <63115824+Matarratas777@users.noreply.github.com>

--- src/screens/Details.tsx
+++ src/screens/Details.tsx
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import { ScrollView, View } from 'react-native';
+import OWMA from '../clients/OWMA';
 import CButton from '../components/CButton';
+import CWWidget from '../components/CWWidget';
+import CWInfo from '../components/CWInfo';
+import CWDetails from '../components/CWDetails';
 import DetailsStyles from '../styles/screens/Details';
 import ThemeContext from '../theme/context';
 import { IDetailsProps, IDetailsState } from '../types/screens/Details';
@@ -14,6 +18,8 @@ export default class Details extends Component<IDetailsProps, IDetailsState> {
     super(props);
     this.state = {
       following: false,
+      location: undefined,
+      conditions: undefined
     };
   }
 
@@ -32,15 +38,80 @@ export default class Details extends Component<IDetailsProps, IDetailsState> {
     });
   };
 
-  componentDidMount() {
+  getData = async () => {
+    this.setState({
+      location: await OWMA.getLocation({
+        coordinates: {
+          lat: 19.24997,
+          lon: -103.72714,
+        },
+      })
+    });
+    if (this.state.location) {
+      this.setState({
+        location: await OWMA.getLocation({
+          coordinates: {
+            lat: 19.24997,
+            lon: -103.72714,
+          },
+        })
+      });
+      this.setState({
+        conditions: {
+          current: await OWMA.getCurrent({
+            coordinates: {
+              lat: 19.24997,
+              lon: -103.72714,
+            },
+          }),
+          forecast: await OWMA.getForecast(10, {
+            coordinates: {
+              lat: 19.24997,
+              lon: -103.72714,
+            },
+          })
+        }
+      });
+    }
+  };
+
+  async componentDidMount() {
     this.setUpAddButton(this.state.following);
+    await this.getData();
   }
 
   render() {
-    return (
-      <ScrollView style={DetailsStyles(this.context).screen.style.container}>
-        <View style={DetailsStyles(this.context).screen.style.content}></View>
-      </ScrollView>
-    );
+    if (this.state.conditions && this.state.location) {
+      return (
+        <ScrollView style={DetailsStyles(this.context).screen.style.container}>
+          <View style={DetailsStyles(this.context).screen.style.content}>
+            <CWWidget
+              style={DetailsStyles(this.context).weatherWidget}
+              data={{
+                city: this.state.location,
+                degrees: this.state.conditions.current.weather.temp.cur,
+                icon: this.state.conditions.current.weather.icon.url,
+              }}
+            />
+            <View style={DetailsStyles(this.context).screen.style.contendInfo}>
+              <CWInfo data={this.state.conditions.current} style={DetailsStyles(this.context).weatherInfo} />
+            </View>
+            <View>
+              <CWDetails data={this.state.conditions.forecast} style={DetailsStyles(this.context).weatherDetails} />
+            </View>
+          </View>
+        </ScrollView>
+      );
+    }
+
+
+
+
+
+
+
+
+
+    
   }
 }
